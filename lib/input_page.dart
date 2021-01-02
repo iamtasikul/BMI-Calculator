90b5df0378eb7b5b0f18bb000b998eb76a1801b8
Tasikul Islam <iamtasikul@gmail.com>
2021-01-02 23:21:21 +0530

    Creating Custom Flutter Widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -25,11 +28,13 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                 child: ReusableCard(
                   colour: activeCardColor,
+                  cardChild: IconContent(icon:FontAwesomeIcons.mars,label: 'MALE'),
                 ),
               ),
               Expanded(
                   child: ReusableCard(
                 colour: activeCardColor,
+                cardChild: IconContent(icon:FontAwesomeIcons.venus,label: 'FEMALE'),
               )),
             ],
           )),
@@ -61,18 +66,6 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
-  final Color colour;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
-  }
-}
+
+
