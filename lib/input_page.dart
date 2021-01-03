1053070ac65afafa2423ad9ca22ee5144e3c272e
Tasikul Islam <iamtasikul@gmail.com>
2021-01-03 14:18:11 +0530

    Navigation added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,10 @@
+import 'package:bmi_calculator/result_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
 import 'constants.dart';
+import 'result_page.dart';
 
 enum Gender {
   male,
@@ -196,10 +198,16 @@ class _InputPageState extends State<InputPage> {
               )),
             ],
           )),
-          Container(
-            color: kBottomContainerColor,
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context,MaterialPageRoute(builder: (context) => ResultPage()),);
+            },
+            child: Container(
+              child: Text('CALCULATE'),
+              color: kBottomContainerColor,
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           )
         ],
       ),
